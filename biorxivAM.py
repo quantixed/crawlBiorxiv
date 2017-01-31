1ca7227d016302f9539477ecf2770a8f5b3ac56f
Stephen Royle <s.j.royle@warwick.ac.uk>
2017-01-31 22:28:26 +0000

    doi2AM working

--- biorxivAM.py
+++ biorxivAM.py
@@ -2,22 +2,12 @@
 from altmetric import Altmetric
 
 def getAMFromDOI(link):
-    try:
-        rsp = a.doi(link)
-        if rsp is None:
-            print "DOI not found"
-        else:
-            out = rsp['score']
-    except AltmetricHTTPException, e:
-#        if e.status_code == 403:
-#            print "You aren't authorized for this call"
-#        elif e.status_code == 420:
-#            print "You are being rate limited"
-#        elif e.status_code == 502:
-#            print "The API version you are using is currently down for maintenance."
-#        elif e.status_code == 404:
-#            print "Invalid API function"
-            out = e.msg
+    rsp = a.doi(link)
+    out = "a"
+    if rsp is None:
+        print "DOI not found"
+    else:
+        out = str(rsp['score'])
     return out
 
 a = Altmetric()
